2f1fa62c48e804b463227dbed19f60e7943070c4
wdkkou <koukouki7547@gmail.com>
2021-11-16 18:07:05 +0900

    feat: GET,POSTができるレベルの実装

--- sample-api/server.go
+++ sample-api/server.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
@@ -13,5 +16,86 @@ func main() {
 		return c.String(http.StatusOK, "Hello, world!")
 	})
 
+	e.GET("/name/:name", getName)
+	e.GET("/show", show)
+	e.POST("/save", save)
+	e.POST("/saveMultipart", saveMultipart)
+	e.POST("/users", getJson)
+
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+// Path Prameters
+func getName(c echo.Context) error {
+
+	name := c.Param("name")
+	return c.String(http.StatusOK, name)
+}
+
+// Query Parameters
+func show(c echo.Context) error {
+
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+
+	s := fmt.Sprintf("team: %s, member: %s", team, member)
+	return c.String(http.StatusOK, s)
+}
+
+// Form application/x-www-form-urlencoded
+func save(c echo.Context) error {
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	s := fmt.Sprintf("name: %s, email: %s", name, email)
+	return c.String(http.StatusOK, s)
+}
+
+// Form multipart/form-data
+func saveMultipart(c echo.Context) error {
+	name := c.FormValue("name")
+
+	text, err := c.FormFile("text")
+	if err != nil {
+		return err
+	}
+
+	src, err := text.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dst, err := os.Create(text.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return c.String(http.StatusOK, "Hi, "+name)
+}
+
+type User struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
+
+type CreateUser struct {
+	Id    int
+	Name  string
+	Email string
+}
+
+func getJson(c echo.Context) error {
+	u := new(User)
+	// Content-Typeヘッダーに基づいて, バインドされる
+	// ex. applicatin/jsonの場合、{"name": "hoge", "email": "huga"}のデータがバインドされる
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	user := CreateUser{Id: 0, Name: u.Name, Email: u.Email}
+	return c.JSON(http.StatusCreated, user)
+
 }
